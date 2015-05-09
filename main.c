f0c39969607edb0e0ccbe1d04326ab48d57e86fb
Nick Albers <nickalbers94@gmail.com>
2015-05-09 19:13:30 +1200

    Third Commit

--- main.c
+++ main.c
@@ -4,53 +4,85 @@
 // Created on: May 8, 2015
 // Author: Nick
 //*****************************************************************************
-
-//Include drivers
+// ADC Header
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
 #include "inc/hw_ints.h"
+#include "inc/lm3s1968.h"
 #include "driverlib/adc.h"
+#include "driverlib/pwm.h"
 #include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/systick.h"
 #include "driverlib/interrupt.h"
 #include "driverlib/debug.h"
 #include "drivers/rit128x96x4.h"
-#include "inc/lm3s1968.h"
-
 
 //Include standard C library files
 #include "stdio.h"
 #include "stdlib.h"
 #include "stdint.h"
+#include "circBuf.h"
 
 //Include program modules
-#include "altitudeMonitor.h"
+//#include "altitudeMonitor.h"
 #include "pwmOutput.h"
 #include "quadDecoder.h"
 #include "circBuf.h"
+
+//Define Constants
+#define BUF_SIZE 10
+#define SAMPLE_RATE_HZ 10
 
 //Declare global strings
 char yawStr[] = "Yaw";
 char altStr[] = "Alt";
 
 //Globals
+static circBuf_t g_AltitudeBuff;		    // Buffer of size BUF_SIZE integers (sample values)
+static unsigned long gul_AltitudeSampCnt;	// Counter for the interrupts
 static uint16_t ul_AltitudeVal;
-circBuf_t g_AltitudeBuff;		    // Buffer of size BUF_SIZE integers (sample values)
-unsigned long gul_AltitudeSampCnt;	// Counter for the interrupts
+//circBuf_t g_AltitudeBuff;		    // Buffer of size BUF_SIZE integers (sample values)
+//unsigned long gul_AltitudeSampCnt;	// Counter for the interrupts
 
 //Function Prototypes
 void Init_Clock (void);
 void Init_Display (void);
 void Init_Ref_Pin (void);
 void Display_Val (volatile int16_t value, char* metric, int y_Pos);
+void Init_ADC(void);
+void Altitude_Monitor_ISR(void);
+void SysTickIntHandler(void);
+
+////*****************************************************************************
+//// Initialise system clock
+////*****************************************************************************
+//void Init_Clock (void)
+//{
+//
+	// Set the clock rate. From Section 19.1 in stellaris_peripheral_lib_UG.doc:
+	//  "In order to use the ADC, the PLL must be used; the PLL output will be
+	//  used to create the clock required by the ADC." ADC rate = 8 MHz / 10.
+//	SysCtlClockSet(SYSCTL_SYSDIV_10 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
+//                   SYSCTL_XTAL_8MHZ);
+//
+	// Set up the period for the SysTick timer.  The SysTick timer period is
+	// set as a function of the system clock.
+//	SysTickPeriodSet(SysCtlClockGet() / SAMPLE_RATE_HZ);
+//	//
+//	// Register the interrupt handler
+//    SysTickIntRegister(Sys_Tick_ISR);
+//	//
+//	// Enable interrupt and device
+//	SysTickIntEnable();
+//    SysTickEnable();
+//}
 
 //*****************************************************************************
 // Initialise system clock
 //*****************************************************************************
 void Init_Clock (void)
 {
-
 	// Set the clock rate. From Section 19.1 in stellaris_peripheral_lib_UG.doc:
 	//  "In order to use the ADC, the PLL must be used; the PLL output will be
 	//  used to create the clock required by the ADC." ADC rate = 8 MHz / 10.
@@ -60,11 +92,9 @@ void Init_Clock (void)
 	// Set up the period for the SysTick timer.  The SysTick timer period is
 	// set as a function of the system clock.
 	SysTickPeriodSet(SysCtlClockGet() / SAMPLE_RATE_HZ);
-	//
-	// Register the interrupt handler
-    SysTickIntRegister(Sys_Tick_ISR);
-	//
-	// Enable interrupt and device
+
+	// Register the interrupt handler and enable interrupts and the device
+    SysTickIntRegister(SysTickIntHandler);
 	SysTickIntEnable();
     SysTickEnable();
 }
@@ -75,6 +105,55 @@ void Init_Clock (void)
 void Init_Display (void)
 {
 	RIT128x96x4Init(1000000);
+}
+
+//*****************************************************************************
+// Initialise ADC
+//*****************************************************************************
+
+void Init_ADC(void)
+{
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+	ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_PROCESSOR, 0);
+	ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH0 | ADC_CTL_IE |
+	                             ADC_CTL_END);
+	ADCSequenceEnable(ADC0_BASE, 3);
+	ADCIntRegister (ADC0_BASE, 3, Altitude_Monitor_ISR);
+	ADCIntEnable(ADC0_BASE, 3);
+}
+
+//*****************************************************************************
+//The Interrupt handler for the Altitude Monitor
+//*****************************************************************************
+
+void Altitude_Monitor_ISR(void)
+{
+	unsigned long ulValue;
+	static int altitudeworking = 0;
+
+	//Get a single sample from ADC0
+	ADCSequenceDataGet(ADC0_BASE, 3, &ulValue);
+
+	//Place it in a circular buffer and advance write index
+	altitudeworking ++;
+	ul_AltitudeVal = (int) ulValue;
+	g_AltitudeBuff.data[g_AltitudeBuff.windex] = (int) ulValue;
+	g_AltitudeBuff.windex ++;
+	if (g_AltitudeBuff.windex >= g_AltitudeBuff.size)
+		g_AltitudeBuff.windex = 0;
+
+	// Clear the interrupt
+	ADCIntClear(ADC0_BASE, 3);
+
+}
+
+void SysTickIntHandler(void)
+{
+    //
+    // Initiate a conversion
+    //
+    ADCProcessorTrigger(ADC0_BASE, 3);
+    gul_AltitudeSampCnt++;
 }
 
 //*****************************************************************************
@@ -107,17 +186,17 @@ void Init_Ref_Pin (void)
 //*****************************************************************************
 // Function to display the mean ADC value (10-bit value, note) and sample count
 //*****************************************************************************
-//void displayMeanVal(int meanVal, int count)
-//{
-//	char string[100];
-//
-//    RIT128x96x4StringDraw("ADC0 (Pin 62)", 5, 24, 15);
-//    RIT128x96x4StringDraw("sampled at 10 Hz", 5, 34, 15);
-//	sprintf(string, "Mean value = %d %", meanVal & 0X03FF);
-//    RIT128x96x4StringDraw(string, 5, 44, 15);
-//	sprintf(string, "Count = %d %", count & 0X0FFF);
-//    RIT128x96x4StringDraw(string, 5, 54, 15);
-//}
+void displayMeanVal(int meanVal, int count)
+{
+	char string[100];
+
+    RIT128x96x4StringDraw("ADC0 (Pin 62)", 5, 24, 15);
+    RIT128x96x4StringDraw("sampled at 10 Hz", 5, 34, 15);
+	sprintf(string, "Mean value = %d %", meanVal & 0X03FF);
+    RIT128x96x4StringDraw(string, 5, 44, 15);
+	sprintf(string, "Count = %d %", count & 0X0FFF);
+    RIT128x96x4StringDraw(string, 5, 54, 15);
+}
 
 
 //*****************************************************************************
@@ -133,33 +212,25 @@ int main (void)
 	Init_Ref_Pin ();
 	initCircBuf (&g_AltitudeBuff, BUF_SIZE);
 
-    // Set the period and pulse width of PWM
-    Set_PWM(DEFAULT_RATE_HZ, DEFAULT_DUTY);
+	// Set the period and pulse width of PWM
+	Set_PWM(DEFAULT_RATE_HZ, DEFAULT_DUTY);
 
-	//uint64_t ul_ADC0Value = 0;
     int sum;
-    int i;
+    unsigned int i;
 
 	//Enable processor interrupts.
 	IntMasterEnable ();
-
 	while (1) // While both pins are high
 	{
 		sum = 0;
-
 		for (i = 0; i < BUF_SIZE; i++)
 		{
 			sum = sum + readCircBuf (&g_AltitudeBuff);
 		}
 
-		Display_Val (Get_Yaw(),yawStr, 5);
-		Display_Val ((sum / BUF_SIZE),altStr, 15);
-		//displayMeanVal(sum / BUF_SIZE, (int) gul_AltitudeSampCnt);
-
-		// Cann comment this out for faster screen updating but with a
-		// side effect of altitude changing rapidly
- 		SysCtlDelay(SysCtlClockGet() / 24);
+		Display_Val (Get_Yaw(), yawStr, 5);
+		Display_Val ((sum / BUF_SIZE), altStr, 15);
+		displayMeanVal(sum / BUF_SIZE, (int) gul_AltitudeSampCnt);
 	}
 }
-
 
