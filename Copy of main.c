67dc913e4bfefdc661816e2c8503642b594c196f
Nick Albers <nickalbers94@gmail.com>
2015-05-09 23:16:56 +1200

    Functional Modular Program
    Quadrature Encoder, ADC and PWM all working,

--- Copy of main.c
+++ Copy of main.c
@@ -4,25 +4,25 @@
 // Created on: May 8, 2015
 // Author: Nick
 //*****************************************************************************
-// ADC Header
+
+//Include drivers
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
 #include "inc/hw_ints.h"
-#include "inc/lm3s1968.h"
 #include "driverlib/adc.h"
-#include "driverlib/pwm.h"
 #include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/systick.h"
 #include "driverlib/interrupt.h"
 #include "driverlib/debug.h"
 #include "drivers/rit128x96x4.h"
+#include "inc/lm3s1968.h"
+
 
 //Include standard C library files
 #include "stdio.h"
 #include "stdlib.h"
 #include "stdint.h"
-#include "circBuf.h"
 
 //Include program modules
 #include "altitudeMonitor.h"
@@ -30,27 +30,27 @@
 #include "quadDecoder.h"
 #include "circBuf.h"
 
-//Include configurations settings
-#include "config.h"
+//Declare global strings
+char yawStr[] = "Yaw";
+char altStr[] = "Alt";
 
-//Define Constants
-#define BUF_SIZE 1
-#define ADC_SAMPLE_RATE_HZ 10
+//Globals
+static uint16_t ul_AltitudeVal;
+circBuf_t g_AltitudeBuff;		    // Buffer of size BUF_SIZE integers (sample values)
+unsigned long gul_AltitudeSampCnt;	// Counter for the interrupts
 
 //Function Prototypes
 void Init_Clock (void);
 void Init_Display (void);
 void Init_Ref_Pin (void);
 void Display_Val (volatile int16_t value, char* metric, int y_Pos);
-void Init_ADC(void);
-void Altitude_Monitor_ISR(void);
-void SysTickIntHandler(void);
 
 //*****************************************************************************
 // Initialise system clock
 //*****************************************************************************
 void Init_Clock (void)
 {
+
 	// Set the clock rate. From Section 19.1 in stellaris_peripheral_lib_UG.doc:
 	//  "In order to use the ADC, the PLL must be used; the PLL output will be
 	//  used to create the clock required by the ADC." ADC rate = 8 MHz / 10.
@@ -59,10 +59,12 @@ void Init_Clock (void)
 
 	// Set up the period for the SysTick timer.  The SysTick timer period is
 	// set as a function of the system clock.
-	SysTickPeriodSet(SysCtlClockGet() / ADC_SAMPLE_RATE_HZ);
-
-	// Register the interrupt handler and enable interrupts and the device
-    SysTickIntRegister(SysTickIntHandler);
+	SysTickPeriodSet(SysCtlClockGet() / SAMPLE_RATE_HZ);
+	//
+	// Register the interrupt handler
+    SysTickIntRegister(Sys_Tick_ISR);
+	//
+	// Enable interrupt and device
 	SysTickIntEnable();
     SysTickEnable();
 }
@@ -85,7 +87,7 @@ void Display_Val (volatile int16_t value, char* metric, int y_Pos)
 	// at the desired positon (X, Y, Level)
 
 	char string [30];
-	sprintf (string, "%s: %d     ", metric, value);
+	sprintf (string, "%s: %d  ", metric, value);
 	RIT128x96x4StringDraw (string, 5, y_Pos, 15);
 }
 
@@ -105,17 +107,17 @@ void Init_Ref_Pin (void)
 //*****************************************************************************
 // Function to display the mean ADC value (10-bit value, note) and sample count
 //*****************************************************************************
-void displayMeanVal(int meanVal, int count)
-{
-	char string[100];
-
-    RIT128x96x4StringDraw("ADC0 (Pin 62)", 5, 24, 15);
-    RIT128x96x4StringDraw("sampled at 10 Hz", 5, 34, 15);
-	sprintf(string, "Mean value = %d   %", meanVal & 0X03FF);
-    RIT128x96x4StringDraw(string, 5, 44, 15);
-	sprintf(string, "Count = %d %", count & 0X0FFF);
-    RIT128x96x4StringDraw(string, 5, 54, 15);
-}
+//void displayMeanVal(int meanVal, int count)
+//{
+//	char string[100];
+//
+//    RIT128x96x4StringDraw("ADC0 (Pin 62)", 5, 24, 15);
+//    RIT128x96x4StringDraw("sampled at 10 Hz", 5, 34, 15);
+//	sprintf(string, "Mean value = %d %", meanVal & 0X03FF);
+//    RIT128x96x4StringDraw(string, 5, 44, 15);
+//	sprintf(string, "Count = %d %", count & 0X0FFF);
+//    RIT128x96x4StringDraw(string, 5, 54, 15);
+//}
 
 
 //*****************************************************************************
@@ -131,43 +133,33 @@ int main (void)
 	Init_Ref_Pin ();
 	initCircBuf (&g_AltitudeBuff, BUF_SIZE);
 
-	// Set the period and pulse width of PWM
-	Set_PWM(PWM_FREQ_HZ, PWM_DEFAULT_DUTY);
+    // Set the period and pulse width of PWM
+    Set_PWM(DEFAULT_RATE_HZ, DEFAULT_DUTY);
 
-	int16_t PWM_Duty;
-    int16_t sum;
-    int16_t avgAltitude;
-    int16_t altitudePercentage;
-    unsigned int i;
+	//uint64_t ul_ADC0Value = 0;
+    int sum;
+    int i;
 
 	//Enable processor interrupts.
 	IntMasterEnable ();
+
 	while (1) // While both pins are high
 	{
 		sum = 0;
+
 		for (i = 0; i < BUF_SIZE; i++)
 		{
 			sum = sum + readCircBuf (&g_AltitudeBuff);
-
 		}
-		// Average the ADC read altitude value across BUF_SIZE counts to maintain
-		// an accurate value, and convert to a  percentage
-		avgAltitude = (sum / BUF_SIZE);
-		altitudePercentage = (avgAltitude * ALTITUDE_SCALE);
 
-		// Set the duty cycle of the PWM relative to the altitude, with
-		// max = 95% and min 5%
-		PWM_Duty = Calc_PWM_Duty(avgAltitude);
-		Set_PWM(PWM_FREQ_HZ, PWM_Duty);
+		Display_Val (Get_Yaw(),yawStr, 5);
+		Display_Val ((sum / BUF_SIZE),altStr, 15);
+		//displayMeanVal(sum / BUF_SIZE, (int) gul_AltitudeSampCnt);
 
-		Display_Val (Get_Yaw(), "Yaw", 5);
-		Display_Val ((avgAltitude), "Altitude", 15);
-		Display_Val ((altitudePercentage), "Altitude %", 55);
-		Display_Val (gul_AltitudeSampCnt, "Count", 25);
-		Display_Val (PWM_FREQ_HZ, "PWM Freq", 35);
-		Display_Val (PWM_Duty, "PWM Duty", 45);
-
-//		displayMeanVal(sum / BUF_SIZE, (int) gul_AltitudeSampCnt);
+		// Cann comment this out for faster screen updating but with a
+		// side effect of altitude changing rapidly
+ 		SysCtlDelay(SysCtlClockGet() / 24);
 	}
 }
+
 
--- altitudeMonitor.h
+++ altitudeMonitor.h
@@ -1,41 +1,27 @@
-/*
- * altitudeMonitor.h
- *
- *  Created on: May 8, 2015
- *      Author: Nick
- */
+//*****************************************************************************
+// altitudeMonitorHome.h
+//
+// Created on: May 9, 2015
+// Author: Nick
+//*****************************************************************************
 
-#ifndef ALTITUDEMONITOR_H_
-#define ALTITUDEMONITOR_H_
+#ifndef ALTITUDEMONITORHOME_H_
+#define ALTITUDEMONITORHOME_H_
 
-//Includes
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
-#include "inc/hw_ints.h"
-#include "driverlib/adc.h"
-#include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
-#include "driverlib/systick.h"
-#include "driverlib/interrupt.h"
-#include "driverlib/debug.h"
-#include "drivers/rit128x96x4.h"
-#include "inc/lm3s1968.h"
-#include "stdio.h"
-#include "stdint.h"
+#include "driverlib/adc.h"
+#include "altitudeMonitor.h"
 #include "circBuf.h"
 
-//Function Prototypes
-void Init_ADC(void);
-void Altitude_Monitor_ISR(void);
-void Sys_Tick_ISR(void);
-void displayMeanVal(int meanVal, int count);
-unsigned long Get_Alt(void);
-
-//Constants
-#define BUF_SIZE 100
-#define SAMPLE_RATE_HZ 150
-
+//Function Variables
 circBuf_t g_AltitudeBuff;		    // Buffer of size BUF_SIZE integers (sample values)
 unsigned long gul_AltitudeSampCnt;	// Counter for the interrupts
 
-#endif /* ALTITUDEMONITOR_H_ */
+//Function prototypes
+void Init_ADC(void);
+void Altitude_Monitor_ISR(void);
+void SysTickIntHandler(void);
+
+#endif /* ALTITUDEMONITORHOME_H_ */
